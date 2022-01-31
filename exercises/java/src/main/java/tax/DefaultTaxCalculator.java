657085f47b70b3b3bfb977a1278e04339761d403
Ben Walker <benjaminjwalker2910@gmail.com>
2022-01-31 16:16:29 +0000

    adding alternative fuel calculation

--- exercises/java/src/main/java/tax/DefaultTaxCalculator.java
+++ exercises/java/src/main/java/tax/DefaultTaxCalculator.java
@@ -1,5 +1,7 @@
 package tax;
 
+import static tax.FuelType.*;
+
 public class DefaultTaxCalculator extends TaxCalculator {
     public DefaultTaxCalculator() {
         super();
@@ -13,6 +15,51 @@ public class DefaultTaxCalculator extends TaxCalculator {
     int calculateTax(Vehicle vehicle) {
         int o2Emis = vehicle.getCo2Emissions();
 
+        switch (vehicle.getFuelType()) {
+            case PETROL:
+                return petrolTax(o2Emis);
+            case DIESEL:
+                return dieselTax(o2Emis);
+            case ALTERNATIVE_FUEL:
+                return altTax(o2Emis);
+            default:
+                return 0;
+        }
+    }
+
+    private int altTax(int o2Emis) {
+        if (o2Emis <= 50) {
+            return 0;
+        } else if (o2Emis <= 75) {
+            return 15;
+        } else if (o2Emis <= 90) {
+            return 95;
+        } else if (o2Emis <= 100) {
+            return 115;
+        } else if (o2Emis <= 110) {
+            return 135;
+        } else if (o2Emis <= 130) {
+            return 155;
+        } else if (o2Emis <= 150) {
+            return 195;
+        } else if (o2Emis <= 170) {
+            return 505;
+        } else if (o2Emis <= 190) {
+            return 820;
+        } else if (o2Emis <= 225) {
+            return 1230;
+        } else if (o2Emis <= 255) {
+            return 1750;
+        } else {
+            return 2060;
+        }
+    }
+
+    private int dieselTax(int o2Emis) {
+        return 0;
+    }
+
+    private int petrolTax(int o2Emis) {
         if (o2Emis == 0) {
             return 0;
         } else if (o2Emis <= 50) {
