653aa4ec09710bb1a94669af669c6a456bfb958b
Ben Walker <benjaminjwalker2910@gmail.com>
2022-01-31 12:31:08 +0000

    refactored petrol tax to pass tests

---
+++ exercises/java/src/main/java/tax/DefaultTaxCalculator.java
@@ -0,0 +1,44 @@
+package tax;
+
+public class DefaultTaxCalculator extends TaxCalculator {
+    public DefaultTaxCalculator() {
+        super();
+    }
+
+    public DefaultTaxCalculator(int year) {
+        super(year);
+    }
+
+    @Override
+    int calculateTax(Vehicle vehicle) {
+        int o2Emis = vehicle.getCo2Emissions();
+
+        if (o2Emis == 0) {
+            return 0;
+        } else if (o2Emis <= 50) {
+            return 10;
+        } else if (o2Emis <= 75) {
+            return 25;
+        } else if (o2Emis <= 90) {
+            return 105;
+        } else if (o2Emis <= 100) {
+            return 125;
+        } else if (o2Emis <= 110) {
+            return 145;
+        } else if (o2Emis <= 130) {
+            return 165;
+        } else if (o2Emis <= 150) {
+            return 205;
+        } else if (o2Emis <= 170) {
+            return 515;
+        } else if (o2Emis <= 190) {
+            return 830;
+        } else if (o2Emis <= 225) {
+            return 1240;
+        } else if (o2Emis <= 255) {
+            return 1760;
+        } else {
+            return 2070;
+        }
+    }
+}
