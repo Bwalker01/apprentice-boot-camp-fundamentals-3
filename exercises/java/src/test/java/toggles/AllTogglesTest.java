fe212c45f91d6e75ec857a4972a870280805c42c
Ben Walker <benjaminjwalker2910@gmail.com>
2022-02-01 13:47:12 +0000

    removing unused imports

--- exercises/java/src/test/java/toggles/AllTogglesTest.java
+++ exercises/java/src/test/java/toggles/AllTogglesTest.java
@@ -2,10 +2,7 @@ package toggles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
-
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class AllTogglesTest {
